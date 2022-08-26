bfee70f352ee28e51177644656ba40ee3086275f
Viet Tran Ba <bowrecurved@gmail.com>
2022-08-26 19:09:25 +0200

    + Noise prediction model

--- diffusion-model-with-mnist-data.py
+++ diffusion-model-with-mnist-data.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 import torch
+import torch.nn as nn
 
 # # Load MNIST Dataset
 
@@ -60,19 +61,100 @@ variances = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
 noisy_images = add_noise(train_dataset.data, variances)
 plot_images(noisy_images)
 
-variances = torch.ones(9) * 0.75
+variances = torch.ones(900) * 0.015
 image = train_dataset.data[0]
 noisy_images = torch.zeros([9, image.shape[0], image.shape[1]])
 for i in range(9):
-    noisy_image = add_noise(image, variances[:i+1])
+    noisy_image = add_noise(image, variances[:i * 100 + 1])
     noisy_images[i] = noisy_image
 plot_images(noisy_images)
+
+
+# # Noise Prediction Model Implementation
+# Uses a UNET architecture to predict the noise of given noisy images.
+
+# +
+class Conv2dBlock(nn.Module):
+    def __init__(self, amount_channels_input, amount_channels_output):
+        super(Conv2dBlock, self).__init__()
+        self.layers = []
+        self.layers.append(nn.Conv2d(amount_channels_input, amount_channels_output, 3, padding=1))
+        self.layers.append(nn.BatchNorm2d(amount_channels_output))
+        self.layers.append(nn.Conv2d(amount_channels_output, amount_channels_output, 3, padding=1))
+        self.layers.append(nn.BatchNorm2d(amount_channels_output))
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, amount_channels_input, amount_channels_output):
+        super(EncoderBlock, self).__init__()
+        self.conv_block = Conv2dBlock(amount_channels_input, amount_channels_output)
+        self.downsampling_layer = nn.MaxPool2d(2)
+        
+    def forward(self, x):
+        x = self.conv_block(x)
+        skip_values = x
+        x = self.downsampling_layer(x)
+        return x, skip_values
+
+    
+class DecoderBlock(nn.Module):
+    def __init__(self, amount_channels_input, amount_channels_output):
+        super(DecoderBlock, self).__init__()
+        self.conv_block = Conv2dBlock(amount_channels_output + amount_channels_input, amount_channels_output)
+        self.upsampling_layer = nn.ConvTranspose2d(amount_channels_input, amount_channels_output, 2, 2)
+        
+    def forward(self, x, skip_values):
+        x = self.upsampling_layer(x)
+        x = torch.cat([x, skip_values], axis=1)
+        x = self.conv_block(x)
+        return x
+    
+
+class NoisePredictionUnet(nn.Module):
+    def __init__(self, amount_channels):
+        super(NoisePredictionUnet, self).__init__()
+        self.layers = []
+        amount_channels_with_timestep = amount_channels + 1
+        self.layers.append(EncoderBlock(amount_channels_with_timestep, amount_channels * 2))
+        self.layers.append(Conv2dBlock(amount_channels * 2, amount_channels * 2))
+        self.layers.append(DecoderBlock(amount_channels * 2, amount_channels))
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, noisy_images, timestep):
+        x = torch.cat([noisy_images, torch.ones(noisy_images.shape) * timestep], axis = 1)
+        x, skip_values = self.layers[0](x)
+        x = self.relu(x)
+        x = self.layers[1](x)
+        x = self.relu(x)
+        x = self.layers[2](x, skip_values)
+        x = self.sigmoid(x)
+        return x
+
+
+# -
 
 # # Training
 
 batch_size = 100
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+model = NoisePredictionUnet(1)
+for data, labels in train_loader:
+    timestep = 3
+    predicted_noises = model(data, 0)
+    assert predicted_noises.shape == data.shape
+    break
+
+# +
+variances = torch.ones(1000) * 0.015
+timesteps = torch.Tensor(range(variances.shape[0]))
 
 epochs = 10
 for epoch_index in range(epochs):
